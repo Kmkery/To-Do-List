9e2db1d8e8c68bd8bd0a3eb334c450969726b3f6
Kmkery <93286646+Kmkery@users.noreply.github.com>
2022-01-10 13:45:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const addForm = document.querySelector('.add-form')
+const todos = document.querySelector('.todos')
+const searchForm = document.querySelector('.search-form')
+
+const addTodo = event => {
+    event.preventDefault()
+    const newTodo = event.target.add.value.trim()
+
+    todos.innerHTML +=`<li class="todo">
+    <span class="text-todo">${newTodo}</span>
+    <i class="delete bi bi-trash"></i>
+    </li>`
+    addForm.reset()
+}
+
+const searchTodo = ({target:{value:inputValue}}) => {
+    const todosArray = Array.from(todos.children)
+
+
+    todosArray.forEach(todo => {
+        const inputIsNotInTodos = !todo.textContent.toLowerCase().includes(inputValue.toLowerCase())
+
+        if(inputIsNotInTodos) {
+            todo.classList.add('hide')
+        } else {
+            todo.classList.remove('hide')
+        }
+    })
+}
+
+const removeTodo = ({target}) => {
+    const classOfClickedElements = Array.from(target.classList)
+    if(classOfClickedElements.includes('delete')) {
+        target.parentNode.remove()
+    }
+}
+
+addForm.addEventListener('submit', addTodo)
+
+searchForm.addEventListener('input', searchTodo)
+
+todos.addEventListener('click', removeTodo)
